542814bf5cff28a4d90c77ef26f5ae0cc71bcd6d
Jamily Lima <jamilylima70@gmail.com>
2022-07-06 18:40:55 -0300

    feat:creat user edit page

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,5 +1,6 @@
 import { Route, Switch } from "react-router-dom";
 import CadastroUser from "../Pages/CadastroUser";
+import EditUser from "../Pages/EditUser";
 import Home from "../Pages/Home";
 import LoginHospital from "../Pages/LoginHospital";
 import LoginUser from "../Pages/LoginUser";
@@ -18,6 +19,9 @@ function Routes() {
       </Route>
       <Route exact path="/CadastroUser">
         <CadastroUser/>
+      </Route>
+      <Route exact path="/EditUser">
+        <EditUser/>
       </Route>
     </Switch>
   );
