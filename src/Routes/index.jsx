9b80aee96bc8301bf7469a60c079e30cca4f2ec6
God bless your rotten soul <caionovo10@gmail.com>
2022-07-06 19:39:58 -0300

    feat: Requirements pag created

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,11 +1,16 @@
 import { Route, Switch } from "react-router-dom";
 import Home from "../Pages/Home";
+import Requirements from "../Pages/Requirements";
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+
+      <Route exact path="/requirements">
+    <Requirements/>
       </Route>
     </Switch>
   );
