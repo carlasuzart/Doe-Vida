3f83a95d580675dbb104a061a8ce2d4352e0b413
Victor Cazuriaga <vikktorrv@gmail.com>
2022-07-07 16:24:17 -0300

    fix: add then and catch response/request

--- src/services/FakeApi.js
+++ src/services/FakeApi.js
@@ -2,15 +2,21 @@ import axios from "axios";
 const baseUrl = "https://apicapstonejson.herokuapp.com";
 
 export const registerUser = (data) =>
-  axios.post({
-    url: `${baseUrl}/register`,
-    method: "POST",
-    data: data,
-  });
+  axios
+    .post({
+      url: `${baseUrl}/register`,
+      method: "POST",
+      data: data,
+    })
+    .then((resp) => console.log(resp))
+    .catch((error) => console.log(error));
 
 export const loginUser = (data) =>
-  axios.post({
-    url: `${baseUrl}/login`,
-    method: "POST",
-    data: data,
-  });
+  axios
+    .post({
+      url: `${baseUrl}/login`,
+      method: "POST",
+      data: data,
+    })
+    .then((resp) => console.log(resp))
+    .catch((error) => console.log(error));
