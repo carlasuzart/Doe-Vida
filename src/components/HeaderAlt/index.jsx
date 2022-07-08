bd9c2fc392d7d38ba2dcc03cb47bbc9c9a1c6963
Fernando Scramignon Narde <scramignonnarde@gmail.com>
2022-07-08 13:25:06 -0300

    feat: adds logout functionality

--- src/components/HeaderAlt/index.jsx
+++ src/components/HeaderAlt/index.jsx
@@ -1,12 +1,20 @@
 import { Container } from "./style";
+import { useHistory } from "react-router-dom";
 
 function HeaderAlt({ type }) {
+  const history = useHistory();
+
+  function handleLogout() {
+    localStorage.clear();
+    history.push("/");
+  }
+
   return (
     <Container type={type}>
       <h1>
         Doe <span>Vida</span>
       </h1>
-      <button>sair</button>
+      <button onClick={handleLogout}>sair</button>
     </Container>
   );
 }
