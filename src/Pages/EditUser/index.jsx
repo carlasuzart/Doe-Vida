5dcf565576db37ba2e749990449a6b75e0874de7
Fernando Scramignon Narde <scramignonnarde@gmail.com>
2022-07-08 16:35:28 -0300

    fix: removes default value of input

--- src/Pages/EditUser/index.jsx
+++ src/Pages/EditUser/index.jsx
@@ -5,21 +5,46 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Link } from "react-router-dom";
 
-
 function EditUser() {
-
-
   const formSchema = yup.object().shape({
     name: yup.string().required("Este campo é obrigatório"),
-    dataNacimento: yup.string().required("Este campo é obrigatório").matches("^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/[12][0-9]{3}$" ,"Formato da data:dd/mm/aaaa"),
-    cpf: yup.string().required("Este campo é obrigatório").matches("[0-9]{3}.?[0-9]{3}.?[0-9]{3}-?[0-9]{2}", "Formato para CPF:000.000.000-00"),
+    dataNacimento: yup
+      .string()
+      .required("Este campo é obrigatório")
+      .matches(
+        "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/[12][0-9]{3}$",
+        "Formato da data:dd/mm/aaaa"
+      ),
+    cpf: yup
+      .string()
+      .required("Este campo é obrigatório")
+      .matches(
+        "[0-9]{3}.?[0-9]{3}.?[0-9]{3}-?[0-9]{2}",
+        "Formato para CPF:000.000.000-00"
+      ),
     endereco: yup.string().required("Este campo é obrigatório"),
-    telefone: yup.string().required("Este campo é obrigatório").matches("^([1-9]{2}) [9]{0,1}[6-9]{1}[0-9]{3}-[0-9]{4}$","Formato do telefone :00 0000-0000"),
-    email: yup.string().required("Este campo é obrigatório").email("Email inválido"),
-    senha: yup.string().required("Este campo é obrigatório").matches(
-      "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$",
-      " A senha deve conter 8 caracteres, uma letra maiúscula, um número e um caractere especial!"),
-    confirmarSenha: yup.string().required("Este campo é obrigatório").oneOf([yup.ref("senha"), null], "As senhas devem corresponder"),
+    telefone: yup
+      .string()
+      .required("Este campo é obrigatório")
+      .matches(
+        "^([1-9]{2}) [9]{0,1}[6-9]{1}[0-9]{3}-[0-9]{4}$",
+        "Formato do telefone :00 0000-0000"
+      ),
+    email: yup
+      .string()
+      .required("Este campo é obrigatório")
+      .email("Email inválido"),
+    senha: yup
+      .string()
+      .required("Este campo é obrigatório")
+      .matches(
+        "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$",
+        " A senha deve conter 8 caracteres, uma letra maiúscula, um número e um caractere especial!"
+      ),
+    confirmarSenha: yup
+      .string()
+      .required("Este campo é obrigatório")
+      .oneOf([yup.ref("senha"), null], "As senhas devem corresponder"),
     check: yup.boolean().isTrue("Você não aceitou os termos!"),
   });
 
@@ -32,7 +57,7 @@ function EditUser() {
   });
 
   const handleChange = (data) => {
-    console.log(data)
+    console.log(data);
   };
 
   return (
@@ -42,14 +67,18 @@ function EditUser() {
       <Container>
         <Content>
           <form onSubmit={handleSubmit(handleChange)}>
-          <h1>Editar</h1>
+            <h1>Editar</h1>
 
             <div>
               <div className="validacao">
                 <label>Nome</label>
                 <p className="erro">{errors.name?.message}</p>
               </div>
-              <input type="text" value="name" placeholder=" Digite seu nome"{...register("name")} />
+              <input
+                type="text"
+                placeholder=" Digite seu nome"
+                {...register("name")}
+              />
             </div>
 
             <div>
@@ -57,7 +86,11 @@ function EditUser() {
                 <label>Data de nascimento</label>
                 <p className="erro">{errors.dataNacimento?.message}</p>
               </div>
-              <input type="text"  value="dataNacimento"  placeholder=" ex: 07/07/1979"{...register("dataNacimento")} />
+              <input
+                type="text"
+                placeholder=" ex: 07/07/1979"
+                {...register("dataNacimento")}
+              />
             </div>
 
             <div>
@@ -65,7 +98,11 @@ function EditUser() {
                 <label>CPF</label>
                 <p className="erro">{errors.cpf?.message}</p>
               </div>
-              <input type="text" value="cpf" placeholder=" ex: 123.456.789-01"{...register("cpf")} />
+              <input
+                type="text"
+                placeholder=" ex: 123.456.789-01"
+                {...register("cpf")}
+              />
             </div>
 
             <div>
@@ -73,7 +110,11 @@ function EditUser() {
                 <label>Email</label>
                 <p className="erro">{errors.email?.message}</p>
               </div>
-              <input type="text" value="email"  placeholder=" Digite seu email"{...register("email")} />
+              <input
+                type="text"
+                placeholder=" Digite seu email"
+                {...register("email")}
+              />
             </div>
 
             <div>
@@ -81,7 +122,11 @@ function EditUser() {
                 <label>Endereço</label>
                 <p className="erro">{errors.endereco?.message}</p>
               </div>
-              <input type="text" value="endereco" placeholder=" Digite seu endereço"{...register("endereco")} />
+              <input
+                type="text"
+                placeholder=" Digite seu endereço"
+                {...register("endereco")}
+              />
             </div>
 
             <div>
@@ -89,7 +134,11 @@ function EditUser() {
                 <label>Telefone</label>
                 <p className="erro">{errors.telefone?.message}</p>
               </div>
-              <input type="text" value="telefone" placeholder=" Digite seu telefone"{...register("telefone")} />
+              <input
+                type="text"
+                placeholder=" Digite seu telefone"
+                {...register("telefone")}
+              />
             </div>
 
             <div>
@@ -97,21 +146,31 @@ function EditUser() {
                 <label>Senha</label>
                 <p className="erro">{errors.senha?.message}</p>
               </div>
-              <input type="password" value="senha" placeholder=" Digite uma senha"{...register("senha")} />
+              <input
+                type="password"
+                placeholder=" Digite uma senha"
+                {...register("senha")}
+              />
             </div>
 
             <div>
               <div className="validacao">
-              <label>Confirmar senha</label>
-              <p className="erro">{errors.confirmarSenha?.message}</p>
-            </div>
-              <input type="password" value="confirmarSenha" placeholder=" Confirme sua senha"{...register("confirmarSenha")} />
+                <label>Confirmar senha</label>
+                <p className="erro">{errors.confirmarSenha?.message}</p>
+              </div>
+              <input
+                type="password"
+                placeholder=" Confirme sua senha"
+                {...register("confirmarSenha")}
+              />
             </div>
 
             <div className="btn">
               <button type="submit">Editar</button>
             </div>
-           <span><Link to="/Schedules">Voltar</Link></span> 
+            <span>
+              <Link to="/Schedules">Voltar</Link>
+            </span>
           </form>
         </Content>
       </Container>
