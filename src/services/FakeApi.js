dc4cef57fef5c18af04cf489b356daff0e29a2e2
Victor Cazuriaga <vikktorrv@gmail.com>
2022-07-11 18:18:23 -0300

    feature: saving Token in the localStorage

--- src/services/FakeApi.js
+++ src/services/FakeApi.js
@@ -1,6 +1,16 @@
 import axios from "axios";
 import { toast } from "react-toastify";
+import React, { createContext } from "react";
+
+//baseUrl
 const baseUrl = "https://apicapstonejson.herokuapp.com";
+
+//save token in localStorage
+const saveToken = (token) => {
+  localStorage.setItem("token", JSON.stringify(token));
+};
+//
+const Context = createContext(0);
 
 export const loginUser = (data) =>
   axios({
@@ -8,30 +18,35 @@ export const loginUser = (data) =>
     method: "POST",
     data: data,
   })
-    .then((resp) => toast.success("Usuário Logando com sucesso"))
+    .then((resp) => {
+      toast.success("Usuário Logado com sucesso");
+      saveToken(resp.data.accessToken);
+    })
     .catch((error) => toast.error(error.response.data));
-    
+
 export const registerUser = (data) =>
-  axios
-  ({
-      url: `${baseUrl}/register`,
-      method: "POST",
-      data: data,
+  axios({
+    url: `${baseUrl}/register`,
+    method: "POST",
+    data: data,
+  })
+    .then((success) => {
+      toast.success("Conta criada com sucesso!");
+      return success;
     })
-    .then((success) => {  
-    toast.success("Conta criada com sucesso!")
-      return success}
-    )
-    .catch((err) =>{
-    
-      if(err.response.data==='Email already exists'){
-        
-         toast.error("Email já cadastrado, tente outro!") 
-   
+    .catch((err) => {
+      if (err.response.data === "Email already exists") {
+        toast.error("Email já cadastrado, tente outro!");
+      } else {
+        toast.error("Ocorreu algum erro. Tente novamente mais tarde");
       }
-      else{
-        toast.error("Ocorreu algum erro. Tente novamente mais tarde")
-      }
-         return err
-    })
-    ;
+      return err;
+    });
+
+export const editUser = (data, id) => {
+  axios({
+    url: `${baseUrl}/register/${id}`,
+    method: "PATCH",
+    data: data,
+  });
+};
