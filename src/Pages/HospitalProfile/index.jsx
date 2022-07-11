4f2ed9021b3c4ac8219eaf11545364898d6d9e47
Jamily Lima <jamilylima70@gmail.com>
2022-07-11 19:58:37 -0300

    fix:Routes button

--- src/Pages/HospitalProfile/index.jsx
+++ src/Pages/HospitalProfile/index.jsx
@@ -1,19 +1,21 @@
 import HeaderAlt from "../../components/HeaderAlt";
 import { Container, Schedules } from "./style";
+import {  useHistory } from "react-router-dom";
 
 function HospitalProfile() {
+  const history = useHistory()
   return (
     <Container>
       <HeaderAlt type="hospital" />
       <div className="hospitalInfo">
         <h2>Hospital da Posse</h2>
         <p>Av. Henrique Duque Estrada Meyer, Nova Iguaçu, Rio de Janeiro</p>
-        <button>e</button>
+        <button onClick={()=>history.push("/EditHospital")}>e</button>
       </div>
       <Schedules>
         <div className="scheduleTitle">
           <h2>Agendamentos</h2>
-          <button>e</button>
+          <button onClick={()=>history.push("/EditHospital")}>e</button>
         </div>
         <div className="scheduleCards">
           <div className="cardItem">
